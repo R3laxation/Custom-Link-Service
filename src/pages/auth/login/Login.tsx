b946e0e03ce7cfbdcdfad699d943adae3c0b70f7
R3laxation <memories@bk.ru>
2022-08-31 22:00:03 +0600

    Added loginForm

--- src/pages/auth/login/Login.tsx
+++ src/pages/auth/login/Login.tsx
@@ -2,21 +2,59 @@ import React from 'react';
 import {Typography, TextField, Button} from "@mui/material";
 
 import styles from './Login.module.scss';
+import {useForm, Controller, SubmitHandler, useFormState} from 'react-hook-form';
+import {loginValidation, passwordValidation} from "./validation";
+
+
+interface ILoginForm {
+    login: string
+    password: string
+}
 
 export const Login: React.FC = () => {
+
+    const {handleSubmit, control} = useForm<ILoginForm>();
+    const {errors} = useFormState({control});
+
+    const onSubmit: SubmitHandler<ILoginForm> = (data) => {
+        console.log(data)
+    }
+
+
     return (
         <div className={styles.loginPage}>
-            <Typography variant="h4" >
+            <Typography variant="h4">
                 Sign in
             </Typography>
             <Typography variant="subtitle1" gutterBottom={true} className={styles.subtitle}>
                 Please enter your login and password
             </Typography>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
 
-                <TextField label={'Login'} size={'small'} margin={'normal'} fullWidth={true}/>
-                <TextField label={'Password'} size={'small'} margin={'normal'} fullWidth={true} type={'password'}/>
-                <Button type={'submit'} variant={'contained'} disableElevation={true} sx={{marginTop: 2}}>
+                <Controller control={control} name={'login'} rules={loginValidation}
+                            render={({field}) => (
+                                <TextField label={'Login'} size={'small'}
+                                           margin={'normal'} fullWidth={true}
+                                           onChange={(e) => field.onChange(e)}
+                                           value={field.value}
+                                           error={!!errors.login?.message}
+                                           helperText={errors.login?.message}
+
+                                />
+                            )}/>
+                <Controller control={control} name={'password'} rules={passwordValidation}
+                            render={({field}) => (
+                                <TextField label={'Password'} size={'small'}
+                                           margin={'normal'} fullWidth={true}
+                                           onChange={(e) => field.onChange(e)}
+                                           value={field.value}
+                                           error={!!errors.password?.message}
+                                           helperText={errors.password?.message}
+
+                                />
+                            )}/>
+                <Button type={'submit'} variant={'contained'} disableElevation={true}
+                        sx={{marginTop: 2}} fullWidth={true}>
                     Sign in
                 </Button>
             </form>
