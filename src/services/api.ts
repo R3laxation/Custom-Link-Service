4e2a0d9b7cfec9d5ad7426826d77c504aa7a393e
R3laxation <memories@bk.ru>
2022-09-01 15:16:40 +0600

    Added two requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,8 +7,7 @@ export const auth = {
         let params = new FormData();
         params.append('username', `${login}`);
         params.append('password', `${password}`);
-        console.log(params)
-        return instance.post(`/login`, {params})
+        return instance.post(`/login`, params)
     },
     register({login, password}: IAuthForm) {
         return instance.post(`/register?username=${login}&password=${password}`)
